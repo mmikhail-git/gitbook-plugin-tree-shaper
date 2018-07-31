d5246f926ce28993c5f526b305c9e3cb50fcd285
Inal Kardanov <kardinal3094@gmail.com>
2018-07-31 20:31:50 +0300

    Init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    book: {
+        assets: './assets',
+        js: [
+            'tree-shaper.js', 'tree-shaper.js'
+        ],
+        css: [
+            'tree-shaper.css', 'tree-shaper.css'
+        ]
+    }
+};
+
