20638fc1db97d20b7c50850d6af46d2bf55fc929
Inal Kardanov <kardinal3094@gmail.com>
2018-08-06 22:02:53 +0300

    Fixes links to github

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
     book: {
         assets: './assets',
         js: [
-            'tree-shaper.js'
+            'tree-shaper.js?cacheBreaker=91ca1700d7a3010e896528cd38d4c291'
         ],
         css: [
             'tree-shaper.css'
